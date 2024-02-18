2b71baef83d27f7436a51bfceb9dc0485bf58a90
Rimon Ahmed <bd.mdrimon2014@gmail.com>
2024-02-19 03:31:33 +0600

    limit 4 tickets

--- js/app.js
+++ js/app.js
@@ -15,48 +15,54 @@ let totalAvailableSeat = 40;
 // const selectedSeat = [];
 const ticketSeat = document.querySelectorAll('.ticket');
 
+let seatCount = 0;
 for (const ticket of ticketSeat ) {
     // console.log(ticket);
    ticket.addEventListener('click', function () {
+       seatCount++;
+       if (seatCount <= 4) {
+           // const ticketText = ticket.innerText;
+           seatCountValue += 1;
+           totalAvailableSeat -= 1;
+
+           // append ticket name
+           const seatDiv = document.getElementById('seat');
+           const ticketName = ticket.innerText;
+           const p = document.createElement('p');
+           p.innerText = ticketName;
+           seatDiv.appendChild(p);
+
+           // append ticket class name
+           const classDiv = document.getElementById('class');
+           const pClass = document.createElement('p');
+           pClass.innerText = className;
+           classDiv.appendChild(pClass);
+           // append price
+           const priceDiv = document.getElementById('price');
+           const pPrice = document.createElement('p');
+           pPrice.innerText = perTicketValue;
+           priceDiv.appendChild(pPrice);
+
+           // get and set total price
+           const totalPrice = parseFloat(document.getElementById('total-price').innerText);
+
+           let sumTotalPrice = (perTicketValue + totalPrice).toFixed(2);
+           document.getElementById('total-price').innerText = sumTotalPrice;
+
+           const grandTotal = document.getElementById('grand-total');
+           grandTotal.innerText = sumTotalPrice;
+
+
+           setInnerText('seat-40', totalAvailableSeat);
+           setInnerText('seat-count', seatCountValue);
+
+           const classBg = ticket.classList.add('bg-[#1DD100]', 'text-white')
+           const classRemove = ticket.classList.remove('bg-gray-200');
+           const classDisable = ticket.setAttribute('disabled', '');
+       } else {
+           alert('You are not allow to purchase more then 4 tickets.')
+       }
        
-        // const ticketText = ticket.innerText;
-        seatCountValue += 1;
-        totalAvailableSeat -= 1;
-
-        // append ticket name
-        const seatDiv = document.getElementById('seat');
-        const ticketName = ticket.innerText;
-        const p = document.createElement('p');
-        p.innerText = ticketName;
-        seatDiv.appendChild(p);
-
-        // append ticket class name
-        const classDiv = document.getElementById('class');
-        const pClass = document.createElement('p');
-        pClass.innerText = className;
-        classDiv.appendChild(pClass);
-        // append price
-        const priceDiv = document.getElementById('price');
-        const pPrice = document.createElement('p');
-        pPrice.innerText = perTicketValue;
-        priceDiv.appendChild(pPrice);
-
-        // get and set total price
-        const totalPrice = parseFloat(document.getElementById('total-price').innerText);
-
-        let sumTotalPrice = (perTicketValue + totalPrice).toFixed(2);
-        document.getElementById('total-price').innerText = sumTotalPrice;
-
-        const grandTotal = document.getElementById('grand-total');
-        grandTotal.innerText = sumTotalPrice;
-
-
-        setInnerText('seat-40', totalAvailableSeat);
-        setInnerText('seat-count', seatCountValue);
-
-        const classBg = ticket.classList.add('bg-[#1DD100]', 'text-white')
-        const classRemove = ticket.classList.remove('bg-gray-200');
-        const classDisable = ticket.setAttribute('disabled', '');
      
     });
 }
